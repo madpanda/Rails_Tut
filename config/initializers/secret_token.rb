952274f5996803e8a49d60cf506975ee06770f7d
Mike Pilla <pilla.mike@gmail.com>
2010-09-09 19:33:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTut::Application.config.secret_token = '31c8cf557cb2214d799c239b27239d2888aa8aaaa711601f75a0d44a1e1ee57c32ec161fb8766faecb58606d1e92e25e8f85d6e63bc74db8b0d00af1b4bcd5a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsTut::Application.config.session_store :cookie_store, :key => '_rails_tut_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# RailsTut::Application.config.session_store :active_record_store
